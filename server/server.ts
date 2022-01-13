8b5dde660c89f6e27c9c7207ad0f85537702087d
kghugo <hugosum.dev@protonmail.com>
2022-01-13 15:11:23 +0000

    Add logger

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,10 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+import logger from "https://deno.land/x/oak_logger@1.0.0/mod.ts";
 
 const app = new Application();
+
+app.use(logger.logger)
+app.use(logger.responseTime)
 
 app.use((ctx) => {
   ctx.response.body = "Hello World!";
