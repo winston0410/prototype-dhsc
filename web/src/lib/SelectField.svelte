6aa8b76bedf206c92d421196e8963c3325c96478
kghugo <hugosum.dev@protonmail.com>
2022-01-19 04:38:57 +0000

    Update

---
+++ web/src/lib/SelectField.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+	import Select from 'svelte-select';
+</script>
+
+<script lang="ts">
+	type SelectItem = {
+		value: string;
+		label: string;
+	};
+
+	export let items: Array<SelectItem>;
+
+	function handleSelect(event) {
+		console.log('selected item', event.detail);
+		// .. do something here 🙂
+	}
+</script>
+
+<Select {items} on:select={handleSelect} />
---
+++ web/src/routes/report-fault/search-location-id.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+	import Button from '$lib/Button.svelte';
+    import InputField from '$lib/InputField.svelte';
+	import SelectField from '$lib/SelectField.svelte';
+	import Separator from '$lib/Separator.svelte';
+	import { errors, validateFields } from '$lib/form';
+    import { onMount } from 'svelte';
+</script>
+
+<script lang="ts">
+    onMount(async () => {
+        await validateFields(["location_id"])
+    })
+</script>
+
+<h1>Fault location – location ID</h1>
+
+<Separator size={40} />
+
+<InputField name={'remember-choice'} type="checkbox">
+    Remember my choice for next time
+</InputField>
+
+<Separator size={40} />
+
+<Button href="./describe-fault" disabled={$errors["location_id"] !== ""}>Next</Button>
