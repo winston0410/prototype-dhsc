b14307c26ac9c1a567c8f9aaefbc02d43cc2df0f
kghugo <hugosum.dev@protonmail.com>
2022-01-21 09:17:37 +0000

    Use vercel for deployment

---
+++ web/.vercel_build_output/static/_app/chunks/dummy-be2169d9.js
@@ -0,0 +1,1 @@
+const a=[{value:"1",label:"Site A"}],l=[{value:"133422",label:"Building A"}];export{l as a,a as d};
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
